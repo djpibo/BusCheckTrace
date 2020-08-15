c4bf0bf4bbaafedeb369e9c51344a669b79080b7
SG <djpibo@naver.com>
2020-08-15 19:38:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/buschecktrace/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.buschecktrace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+    극강의 UX 디자인을 준수할 것
+    객체지향의 원칙을 준수할 것
+    버스 탑승 시간과 도착 시간을 정확히 저장하고 통계를 낼 것
+
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
